38f2b07cb3bc00c7b3703f96e451f149529174ed
hladada <1458773799@QQ.com>
2025-08-01 17:38:16 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        cn.sinosoft.claim.intf.service.impl.regist.SubmitCarRegistImpl#submitNewCarRegist
+    com.thoughtworks.xstream.XStream#toXML(java.lang.Object)
+    org.apache.log4j.Category#info(java.lang.Object)
+    java.lang.System#currentTimeMillis
+    cn.sinosoft.claim.intf.service.impl.regist.SubmitCarRegistImpl#getSubmitCarRegistResponse
+        cn.sinosoft.claim.intf.service.validdata.regist.SubmitCarRegistValidata#requestDataValidte
+        cn.sinosoft.claim.intf.service.util.ServicesExceptionUtils#getErrMsg
+    cn.sinosoft.claim.intf.service.impl.regist.SubmitCarRegistImpl#getRegistNo
+        com.ccic.service.carclaim.submitnewcarregist.bean.SubmitCarRegistRequest#getRequestBody
+        com.ccic.service.carclaim.submitnewcarregist.bean.SubmitCarRegistReqDTO#getPolicyDtoList
+        java.util.List#size
+        java.util.List#get
+        com.ccic.service.carclaim.submitnewcarregist.bean.PolicyDTO#getRegistNo
+    java.lang.System#currentTimeMillis
+    org.apache.log4j.Category#info(java.lang.Object)
+    com.thoughtworks.xstream.XStream#toXML(java.lang.Object)
+    org.apache.log4j.Category#info(java.lang.Object)
+        System.out.println("Hello, World!");
+    }
+}
